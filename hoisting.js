495d5884732ec9a1da358ca21404e79c38743729
Mark Nowowiejski <graphicmarkus@gmail.com>
2021-10-11 10:55:54 -0400

    adding lesson 18-Hoisting

---
+++ hoisting.js
@@ -0,0 +1,17 @@
+var age;
+
+console.log("4, 8, 15, 16, 23, 42");
+console.log(age);
+
+age = 44;
+
+sayHi();
+
+function sayHi() {
+  console.log("Greetings Fools");
+  console.log(add(4, 5));
+}
+
+function add(a, b) {
+  return a + b;
+}
