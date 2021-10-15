d91c1f25ee1ea0b83c029d1190c05551c68ea3c3
Mark Nowowiejski <graphicmarkus@gmail.com>
2021-10-15 13:54:36 -0400

    adding Lesson 24 DOM/CSS'

--- exercises/20 - The DOM/the-dom.js
+++ exercises/20 - The DOM/the-dom.js
@@ -1,23 +1,36 @@
-console.log("How bout them Ravens!");
+// console.log("How bout them Ravens!");
 
-// const imgs = document.querySelectorAll("img");
-// const items2 = document.querySelector(".farts");
+// // const imgs = document.querySelectorAll("img");
+// // const items2 = document.querySelector(".farts");
 
-// console.log(imgs);
-// console.log(items2);
+// // console.log(imgs);
+// // console.log(items2);
 
-const heading = document.querySelector("h2");
-console.log(heading.textContent);
-// heading.textContent = "Markus is on fire!";
-console.log(heading.innerText);
+// const heading = document.querySelector("h2");
+// console.log(heading.textContent);
+// // heading.textContent = "Markus is on fire!";
+// console.log(heading.innerText);
 
-console.log(heading.innerHTML);
-console.log(heading.outerHTML);
+// console.log(heading.innerHTML);
+// console.log(heading.outerHTML);
 
-const pizzaLight = document.querySelector(".pizza");
+// const pizzaLight = document.querySelector(".pizza");
 
-console.log(pizzaLight.textContent);
+// console.log(pizzaLight.textContent);
 
-pizzaLight.textContent = `${pizzaLight.textContent}   and cheese`;
+// pizzaLight.textContent = `${pizzaLight.textContent}   and cheese`;
 
-pizzaLight.insertAdjacentText("beforeend", " bacon");
+// pizzaLight.insertAdjacentText("beforeend", " bacon");
+
+//working with classes
+
+const pic = document.querySelector(".nice");
+pic.classList.remove(".nice");
+pic.classList.add("round");
+console.log(pic.classList);
+
+function toggleRound() {
+  pic.classList.toggle("round");
+}
+
+pic.addEventListener("click", toggleRound);
