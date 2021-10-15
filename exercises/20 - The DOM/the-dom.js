2d188b2046ecbadc7e76cb6af0eac9fd691ea518
Mark Nowowiejski <graphicmarkus@gmail.com>
2021-10-15 13:23:31 -0400

    adding lesson 22 Element Selectors

--- exercises/20 - The DOM/the-dom.js
+++ exercises/20 - The DOM/the-dom.js
@@ -1,7 +1,23 @@
 console.log("How bout them Ravens!");
 
-const imgs = document.querySelectorAll("img");
-const items2 = document.querySelector(".farts");
+// const imgs = document.querySelectorAll("img");
+// const items2 = document.querySelector(".farts");
 
-console.log(imgs);
-console.log(items2);
+// console.log(imgs);
+// console.log(items2);
+
+const heading = document.querySelector("h2");
+console.log(heading.textContent);
+// heading.textContent = "Markus is on fire!";
+console.log(heading.innerText);
+
+console.log(heading.innerHTML);
+console.log(heading.outerHTML);
+
+const pizzaLight = document.querySelector(".pizza");
+
+console.log(pizzaLight.textContent);
+
+pizzaLight.textContent = `${pizzaLight.textContent}   and cheese`;
+
+pizzaLight.insertAdjacentText("beforeend", " bacon");
