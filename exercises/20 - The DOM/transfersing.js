0b41dc45fa5e595fd336c74e44a2f560897c53a6
Mark Nowowiejski <graphicmarkus@gmail.com>
2021-10-25 09:22:46 -0400

    Adding Lesson 27 Transversing DOM and removing Nodes

---
+++ exercises/20 - The DOM/transfersing.js
@@ -0,0 +1,11 @@
+console.log("Hello Darknesss my old friend");
+
+const wes = document.querySelector(".wes");
+
+console.log(wes.children);
+
+console.log(wes.childNodes);
+
+wes.remove();
+
+console.log(wes);
